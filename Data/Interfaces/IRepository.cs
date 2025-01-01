ffa0e4041479330a0212e39b505d9c6060b71255
Giorgi <giorgi.tugushi.1@btu.edu.ge>
2025-01-01 14:44:34 +0400

    Entities created

---
+++ Data/Interfaces/IRepository.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using CmoviesDAL.Data.Entities;
+
+namespace CmoviesDAL.Data.Interfaces
+{
+    public interface IRepository<TEntity> where TEntity : BaseEntity
+    {
+        Task<IEnumerable<TEntity>> GetAllAsync();
+
+        Task<TEntity> GetByIdAsync(int id);
+
+        Task AddAsync(TEntity entity);
+
+        void Delete(TEntity entity);
+
+        Task DeleteByIdAsync(int id);
+
+        void Update(TEntity entity);
+    }
+}
